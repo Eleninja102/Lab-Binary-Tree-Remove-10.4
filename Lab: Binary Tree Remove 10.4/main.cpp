0677942350480f0fede0f87e2bcf1a642208fd0d
Coleton Watt <electricninja102@gmail.com>
2022-03-23 18:49:40 -0600

    Initial Commit

---
+++ Lab: Binary Tree Remove 10.4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab: Binary Tree Remove 10.4
+//
+//  Created by Coleton Watt on 3/23/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
